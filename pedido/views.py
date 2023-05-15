768d0926f74f9c069cd8d4c5e7b62472bef99627
Deivid <deividnaruto@hotmail.com>
2023-05-15 15:53:33 -0300

    adicionando valor final na finalizaçao da compra

--- pedido/views.py
+++ pedido/views.py
@@ -26,10 +26,12 @@ class Pedido_PD(View):
         
         perfil_user = Perfil_Endereco.objects.get(user_perfil=user.id)
         
-        
+        frete = self.request.session.get('frete')
+
         self.contexto = {
             'perfil_usuario': perfil_user, 
-            'carrinho': self.request.session.get('carrinho')
+            'carrinho': self.request.session.get('carrinho'),
+            'preco':frete['preco']
         }
 
     def get(self,*args,**kwargs):
