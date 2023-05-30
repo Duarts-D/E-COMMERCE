bb242aa8e83b99c09f69f54c8489054720399d12
Deivid <deividnaruto@hotmail.com>
2023-05-30 10:42:04 -0300

    adicionando enable local para options para conformidade docker

--- apps/gerador_pdf/views.py
+++ apps/gerador_pdf/views.py
@@ -6,7 +6,6 @@ import pdfkit
 from rolepermissions.decorators import has_permission_decorator
 from apps.pedido.models import Pedido
 
-
 @has_permission_decorator('gerar_pdf')
 def gerador_html(request,pk):
     pedido = Pedido.objects.get(pk=pk)
@@ -15,13 +14,13 @@ def gerador_html(request,pk):
     template = get_template('formulario_retirada_estoque.html')
     html = template.render({'pedido':pedido,'path':path})
 
-    css = 'static/css/styles.css'
+    css = 'setup/static/css/styles.css'
     options = {
+        'enable-local-file-access' : None,
         'page-size': 'A4',
-        'encoding': "UTF-8",
+        'encoding': 'UTF-8',
     }
     pdf = pdfkit.from_string(html,False,css=css,options=options)
     response = HttpResponse(pdf, content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="arquivo.pdf'
     return response
-
